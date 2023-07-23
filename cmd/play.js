40a29b6822b4d58b2bfbec9796961a201be9907b
Klubuntu <local@fix.pc>
2023-07-24 00:22:58 +0200

    Enable support more streaming services

--- cmd/play.js
+++ cmd/play.js
@@ -1,10 +1,10 @@
 const {useMainPlayer, GuildQueue, useQueue} = require("discord-player");
-const { YoutubeExtractor } = require("@discord-player/extractor");
+require("@discord-player/extractor");
 const { getEmoji, getEmbed } = require("../helpers/utils");
 
 async function play(interaction) {
   const player = interaction.player;
-  await player.extractors.register(YoutubeExtractor, {});
+  await player.extractors.loadDefault();
   const channel = interaction.member.voice.channel;
   if (!channel) {
     msg_user_not_voicechannel = "❌ User not joined to voice channel";
@@ -32,7 +32,17 @@ async function play(interaction) {
     await interaction.reply(
       `${getEmoji("search")} **Searching for**: <${query}>`
     );
-    res = await player.search(query);
+    const searchService = interaction.options?._hoistedOptions[1]?.value;
+    const searchServiceName = searchService.charAt(0).toUpperCase() + searchService.slice(1).replaceAll("Search", "");
+    if (searchService && searchService.length > 0) {
+      options = {
+        fallbackSearchEngine: searchService,
+      };
+      res = await player.search(query, options);
+    } else {
+      res = await player.search(query);
+    }
+
     try{
       track_url = res._data.tracks[0].url;
       track_name = res._data.tracks[0].title;
@@ -45,11 +55,10 @@ async function play(interaction) {
     }
 
     if(track_duration == "0:00"){
-      track_duration = "On Live"
+      track_duration = "Live"
     }
-    track_publishDate = res._data.tracks[0].__metadata.uploadedAt || ":record_button: Live";
+    track_publishDate = res._data.tracks[0].__metadata.uploadedAt || "Unavailable";
     thumbnail_url = res._data.tracks[0].thumbnail;
-   //  console.log(res._data.tracks[0]);
     const voiceChannel = interaction.guild.members.cache.get(
       interaction.member.user.id
     ).voice.channelId;
@@ -61,23 +70,37 @@ async function play(interaction) {
       desc: "by Manager :cd: https://manager-discord.netlify.app",
       img: thumbnail_url,
       fields: [
-        { name: "Channel", value: `<#${voiceChannel}>`, inline: true },
+        {name: "Playing from", value: ":headphones: "+searchServiceName || "Spotify", inline: true},
+        // { name: "Channel", value: `<#${voiceChannel}>`, inline: true },
         { name: "Duration", value: track_duration, inline: true },
         { name: "Publish Date", value: track_publishDate, inline: true },
       ],
     };
+    if(res._data.queryType != "youtube"){
+      opt_playMsg.img = null;
+      opt_playMsg.thumb = thumbnail_url
+    }
     try {
       playMsg = getEmbed(opt_playMsg);
       await interaction.channel.send({ embeds: [playMsg] });
     } catch (e) {
       console.error("[BOT] Missing permission for send messages");
     }
-  
-    await player.play(channel, track_url, {
-      nodeOptions: {
-        metadata: interaction,
-      },
-    });
+    try{
+      await player.play(channel, track_url, {
+        nodeOptions: {
+          metadata: interaction,
+        },
+      });
+    }
+    catch{
+      console.log("[BOT] Sorry, This type video/live not supported")
+      opt_playMsg.img = null;
+      opt_playMsg.fields = null;
+      opt_playMsg.desc =  ":x: Sorry, This type video/live not supported"
+      exceptMsg = getEmbed(opt_playMsg);
+      interaction.followUp({embeds: [exceptMsg]})
+    }
   }
 }
 
