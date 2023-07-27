2671b93003221e4a13b9119899c4d2af20235124
Klubuntu <local@fix.pc>
2023-07-27 19:11:55 +0200

    7:11 PM - Future embed version, tmp move to old

--- helpers/embeds_old.js
+++ helpers/embeds_old.js
@@ -1,16 +1,7 @@
 const {getEmoji, getEmbed} = require("./utils");
 const setUserLanguage = require('./lang_parser')
-
-function useEmbed(track, name) {
-   async function getConfig() {
-      const config = await setUserLanguage("en");
-      // Use the config here
-      return config;
-    }
-   x = getConfig().then((x) => {
-         console.log(x); // Output: "Async operation completed!"
-      });
-   console.log(x)
+async function useEmbed(track, name) {
+   const config = await setUserLanguage("en");
    trackSource = track.raw.source || track.__metadata.source
    trackSourceFormatted = trackSource.charAt(0).toUpperCase() + trackSource.slice(1)
    playEmbedData = {
@@ -20,38 +11,38 @@ function useEmbed(track, name) {
       desc: "by Manager :cd: https://manager-discord.netlify.app",
       img: track.thumbnail,
       fields: [
-         {name: "Playing from", value: ":headphones: "+ trackSourceFormatted, inline: true},
-         // { name: "Channel", value: `<#${voiceChannel}>`, inline: true },
-         { name: "Duration", value: track.duration, inline: true },
-         { name: "Publish Date", value: track.__metadata.uploadedAt || "Unknown", inline: true },
+        {name: "Playing from", value: ":headphones: "+ trackSourceFormatted, inline: true},
+        // { name: "Channel", value: `<#${voiceChannel}>`, inline: true },
+        { name: "Duration", value: track.duration, inline: true },
+        { name: "Publish Date", value: track.__metadata.uploadedAt || "Unknown", inline: true },
       ],
    };
    skipEmbedData = {
       color: 0xde703a,
-      title: `${getEmoji("skip")} Skipped Song`
+      title: `${getEmoji("skip")} ${config.messages.skip[0].skipped}`
    }
    if(name == "error"){
       embedName = eval("playEmbedData")
       embedName.img = null;
       embedName.fields= null;
-      embedName.desc = ":x: Sorry, This type video/live not supported"
+      embedName.desc = `:x: ${config.messages.play[6].unsupported}`
 
    }
    else if(name == "add"){
       embedName = eval("playEmbedData")
-      embedName.title = `${getEmoji("clock")} Added to Queue ${track.title}`
+      embedName.title = `${getEmoji("clock")} ${config.messages.queue[2].added} ${track.title}`
    }
    else{
-      embedName = eval(name + "EmbedData")
+      embedName = eval(name + "EmbedData2")
    }
    if(track.source != "youtube"){
       embedName.img = ""
       embedName.thumb = track.thumbnail
    }
    tmpNewEmbed = getEmbed(embedName)
-   return tmpNewEmbed
-   
-   // console.log(config);
+   console.log(tmpNewEmbed)
+   // return tmpNewEmbed
 }
 
 module.exports = {useEmbed: useEmbed}
+
