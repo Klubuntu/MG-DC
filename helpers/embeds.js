22752288ce03663a30ca5ce84526753b041d1206
Klubuntu <local@fix.pc>
2023-07-27 04:41:56 +0200

    4:41 AM - Update embeds

--- helpers/embeds.js
+++ helpers/embeds.js
@@ -1,35 +1,37 @@
 const {getEmoji, getEmbed} = require("./utils");
-
-function useEmbed(track, name) {
+const setUserLanguage = require('./lang_parser')
+async function useEmbed(track, name) {
+   const config = await setUserLanguage("en");
+   console.log(config)
    trackSource = track.raw.source || track.__metadata.source
    trackSourceFormatted = trackSource.charAt(0).toUpperCase() + trackSource.slice(1)
    playEmbedData = {
       color: 0x26d9a0,
-      title: `${getEmoji("music")} Playing ${track.title}`,
+      title: `${getEmoji("music")} ${config.messages.play[1].playing} ${track.title}`,
       url: track.url,
       desc: "by Manager :cd: https://manager-discord.netlify.app",
       img: track.thumbnail,
       fields: [
-        {name: "Playing from", value: ":headphones: "+ trackSourceFormatted, inline: true},
+        {name: config.messages.play[2].playing_from, value: ":headphones: "+ trackSourceFormatted, inline: true},
         // { name: "Channel", value: `<#${voiceChannel}>`, inline: true },
-        { name: "Duration", value: track.duration, inline: true },
-        { name: "Publish Date", value: track.__metadata.uploadedAt || "Unknown", inline: true },
+        { name: config.messages.play[3].duration, value: track.duration, inline: true },
+        { name: config.messages.play[4].publish_date, value: track.__metadata.uploadedAt || config.messages.play[5].unknown, inline: true },
       ],
    };
    skipEmbedData = {
       color: 0xde703a,
-      title: `${getEmoji("skip")} Skipped Song`
+      title: `${getEmoji("skip")} ${config.messages.skip[0].skipped}`
    }
    if(name == "error"){
       embedName = eval("playEmbedData")
       embedName.img = null;
       embedName.fields= null;
-      embedName.desc = ":x: Sorry, This type video/live not supported"
+      embedName.desc = `:x: ${config.messages.play[6].unsupported}`
 
    }
    else if(name == "add"){
       embedName = eval("playEmbedData")
-      embedName.title = `${getEmoji("clock")} Added to Queue ${track.title}`
+      embedName.title = `${getEmoji("clock")} ${config.messages.queue[2].added} ${track.title}`
    }
    else{
       embedName = eval(name + "EmbedData")
