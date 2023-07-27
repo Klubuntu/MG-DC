f510d680a002b6ccb71cde3d365bc4789519b936
Klubuntu <local@fix.pc>
2023-07-27 04:16:42 +0200

    4:17 AM - Change strings to use config

--- cmd/state.js
+++ cmd/state.js
@@ -2,13 +2,14 @@ const {useQueue} = require("discord-player");
 const { getEmoji, getEmbed } = require("../helpers/utils");
 
 async function progress(interaction){
+   const config = interaction.locale_config
    getQueue = useQueue(interaction.guild.id)
    if(getQueue){
       progress_queue = getQueue.node.createProgressBar()
       console.log("[DEBUG] Progress - Queue exists")
       opts_prog = {
          color: 0xeda915,
-         title: `${getEmoji("setup")} Waiting for player start`,
+         title: `${getEmoji("setup")} ${config.messages.progress[0].wait}`,
          url: "https://youtube.com",
          desc: progress_queue
        }
@@ -24,60 +25,63 @@ async function progress(interaction){
       }
 
    }else{
-      interaction.reply(":cd: User not playing songs")
+      interaction.reply(`:cd: ${config.messages.user_not_playing}`)
    }
 }
 
 
 async function pause(interaction) {
+   const config = interaction.locale_config
    getQueue = useQueue(interaction.guild.id)
    if(getQueue){
     console.log("[DEBUG] Pause - Queue exists")
     opts_pause = {
       color: 0xeda915,
-      title: `${getEmoji("pause")} Playing / Queue paused`
+      title: `${getEmoji("pause")} ${config.messages.pause[0].paused}`
     }
     pause = getEmbed(opts_pause)
     interaction.reply({embeds: [pause]})
     await getQueue.node.pause()
    }else{
-      interaction.reply(":cd: User not playing song")
+      interaction.reply(`:cd: ${config.messages.user_not_playing}`)
    }
 }
 
 async function resume(interaction) {
+   const config = interaction.locale_config
    getQueue = useQueue(interaction.guild.id)
    if(getQueue){
     console.log("[DEBUG] Resume - Queue exists")
     opts_resume = {
       color: 0x4d8ceb,
-      title: `${getEmoji("play")} Playing / Queue resumed`
+      title: `${getEmoji("play")} ${config.messages.resume[0].resumed}`
     }
     resume = getEmbed(opts_resume)
     interaction.reply({embeds: [resume]})
     await getQueue.node.resume()
    }else{
-      interaction.reply(":cd: User not have song to play")
+      interaction.reply(`:cd: ${config.messages.resume[1].not_have_song}`)
    }
 }
 
 async function volume(interaction){
+   const config = interaction.locale_config
    getQueue = useQueue(interaction.guild.id)
    if(getQueue){
       volumeLevel = interaction.options.getInteger("level")
       if(!volumeLevel){
-         interaction.reply(":speaker: The current volume set is: " + getQueue.node.volume)
+         interaction.reply(`:speaker: ${config.messages.volume[0].level}: ${getQueue.node.volume}`)
       }
       else if(volumeLevel <= 200){
          await getQueue.node.setVolume(volumeLevel)
-         interaction.reply(":speaker: Changed volume to " + volumeLevel)
+         interaction.reply(`:speaker: ${config.messages.volume[1].set} ${volumeLevel}`)
       }
       else{
-         interaction.reply(":x: User set undefined volume level")
+         interaction.reply(`:x: ${config.messages.volume[2].max_level}`)
       }
    }
    else{
-      interaction.reply(":x: User not playing song")
+      interaction.reply(`:x: ${config.messages.user_not_playing}`)
    }
 }
 
