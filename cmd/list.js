c9f0b8360c6fccd1becda1be6fd8516d814f8c03
Klubuntu <local@fix.pc>
2023-07-31 12:00:50 +0200

    Introduce actions

--- cmd/list.js
+++ cmd/list.js
@@ -4,12 +4,22 @@ const stateFunctions = require('./state')
 const queueFunctions = require('./queue');
 const helpFunctions = require('./help');
 const setUserLanguage = require('../helpers/lang_parser')
+const {playEvent, skipEvent, pauseEvent, resumeEvent, seekEvent, trackAddEvent, stopEvent} = require('../helpers/actions');
 const { logAction } = require('../helpers/utils');
 
 function setupCommands(client) {
    client.on("interactionCreate", async (interaction) => {
       const userLocale = interaction?.locale || 'en';
       interaction.locale_config = await setUserLanguage(userLocale) || await setUserLanguage('en');
+      /* Define Events */
+      interaction.playEvent = playEvent;
+      interaction.skipEvent = skipEvent;
+      interaction.pauseEvent = pauseEvent;
+      interaction.resumeEvent = resumeEvent;
+      interaction.seekEvent = seekEvent;
+      interaction.trackAddEvent = trackAddEvent;
+      interaction.stopEvent = stopEvent;
+      /* ------------- */
       if(interaction.isButton()) {
          btnID = interaction.customId
          if(btnID == "btn_playback"){
--- helpers/actions.js
+++ helpers/actions.js
@@ -1,1 +1,55 @@
-/* Add actions on next release */
+const { useEmbed } = require("./embeds");
+
+class PlayerEvents {
+   playEvent(i) {
+      const track = i.track;
+      track.config = i.locale_config;
+      console.log("[BOT] Playing", track.url);
+      i.channel.send({ embeds: [useEmbed(track, "play")] });
+   }
+
+   trackAddEvent(i) {
+      const track = i.track;
+      track.config = i.locale_config;
+      console.log("[BOT] Added to play", track.url);
+      i.channel.send({ embeds: [useEmbed(track, "add")] }).catch(console.error);
+   }
+
+   skipEvent(i) {
+      const track = i.track;
+      track.config = i.locale_config;
+      console.log("[BOT] Skipped Track", track.url);
+      i.reply({ embeds: [useEmbed(track, "skip")] });
+   }
+
+   pauseEvent(i) {
+      const track = i.track;
+      track.config = i.locale_config;
+      console.log("[BOT] Paused Track", track.url);
+      i.reply({ embeds: [useEmbed(track, "pause")] });
+   }
+
+   resumeEvent(i) {
+      const track = i.track;
+      track.config = i.locale_config;
+      console.log("[BOT] Resumed Track", track.url);
+      i.reply({ embeds: [useEmbed(track, "resume")] });
+   }
+
+   seekEvent(i) {
+      const track = i.track;
+      track.config = i.locale_config;
+      track.other = i.other;
+      console.log("[BOT] Seeked Track", track.url);
+      i.reply({ embeds: [useEmbed(track, "seek")] });
+   }
+
+   stopEvent(i) {
+      const track = i.track;
+      track.config = i.locale_config;
+      console.log("[BOT] Stopped Playing");
+      i.reply({ embeds: [useEmbed(track, "")] });
+   }
+}
+
+module.exports = new PlayerEvents();
